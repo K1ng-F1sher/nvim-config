17488b5a4fbd0a7230c52257aa9d1d94f595f0f7
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-02-13 21:54:18 +0100

    Altered config

--- lua/lars04/init.lua
+++ lua/lars04/init.lua
@@ -1,2 +1,9 @@
 require("lars04.set")
 require("lars04.remap")
+
+vim.cmd[[
+augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=200 }
+augroup END
+]]
