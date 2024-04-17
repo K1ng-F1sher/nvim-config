1f1e88aebb1e65f09d504b180edd047e199bb6b0
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-04-17 21:57:31 +0200

    added telescope config for reference finding

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,11 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', 'gr', builtin.lsp_references, {})
+vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
+vim.keymap.set('n', 'gt', builtin.lsp_type_definitions, {})
+vim.keymap.set('n', 'gq', builtin.quickfix, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
