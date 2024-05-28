93506281db2483cf3c09847f0d407adc02d86e2c
Lars Loonen <larsvdspek@gmail.com>
2024-05-28 22:51:43 +0200

    added gc remap (not wokring yet)

--- lua/lars04/remap.lua
+++ lua/lars04/remap.lua
@@ -4,6 +4,9 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-a>", "ggVG")
+vim.keymap.set("n", "<C-_>", "Vgc")
+vim.keymap.set("v", "<C-_>", "gc")
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
