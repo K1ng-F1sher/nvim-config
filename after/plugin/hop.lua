10f6aa62011c44e6eb6aecd446fce0464bb3aff8
Lars Loonen <larsvdspek@gmail.com>
2024-06-04 21:33:23 +0200

    removed unused hop commands

--- after/plugin/hop.lua
+++ after/plugin/hop.lua
@@ -1,9 +1,9 @@
 local hop = require('hop')
 
 vim.keymap.set("", "S", "<cmd>HopWord<CR>", {noremap=false})
-vim.keymap.set("", "<leader>f", "<cmd>HopChar1AC<CR>", {noremap=false})
-vim.keymap.set("", "<leader>F", "<cmd>HopChar1BC<CR>", {noremap=false})
-vim.keymap.set("", "<leader><leader>f", "<cmd>HopChar2AC<CR>", {noremap=true})
-vim.keymap.set("", "<leader><leader>F", "<cmd>HopChar2BC<CR>", {noremap=true})
+-- vim.keymap.set("", "<leader>f", "<cmd>HopChar1AC<CR>", {noremap=false})
+-- vim.keymap.set("", "<leader>F", "<cmd>HopChar1BC<CR>", {noremap=false})
+-- vim.keymap.set("", "<leader><leader>f", "<cmd>HopChar2AC<CR>", {noremap=true})
+-- vim.keymap.set("", "<leader><leader>F", "<cmd>HopChar2BC<CR>", {noremap=true})
 vim.keymap.set("", "<Leader>j", "<cmd>HopLineAC<CR>", {noremap=true})
 vim.keymap.set("", "<Leader>k", "<cmd>HopLineBC<CR>", {noremap=true})
