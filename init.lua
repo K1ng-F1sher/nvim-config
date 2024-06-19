25c3ce3b6c6364c958ca4a49e39e9dc14fcdf123
Lars Loonen <larsvdspek@gmail.com>
2024-06-19 21:18:58 +0200

    first start at implementing lazy

--- init.lua
+++ init.lua
@@ -1,2 +1,120 @@
-require("lars04")
+-- require("lars04")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+
+require('lazy').setup({
+
+  -- Theme
+  use({ 'rose-pine/neovim',
+  as = 'rose-pine',
+  config = function()
+    require('rose-pine').setup({
+      disable_background = true,
+      disable_italics = true,
+      styles = {
+        italic = false,
+      },
+    })
+
+    vim.cmd("colorscheme rose-pine")
+  end
+  })
+
+  -- (File) Navigation
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use('theprimeagen/harpoon')
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+      --"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  }
+  use { 'fgheng/winbar.nvim' }
+  use({ 'SmiteshP/nvim-navic', as = 'nvim-navic' })
+  use {
+      'smoka7/hop.nvim',
+      tag = '*', -- optional but strongly recommended
+      config = function()
+          require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+      end
+  }
+
+  -- LSP
+  use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v3.x',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
+
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-buffer'},
+          {'hrsh7th/cmp-path'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'hrsh7th/cmp-nvim-lua'},
+          {'saadparwaiz1/cmp_luasnip'},
+
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},
+          {'rafamadriz/friendly-snippets'}
+      }
+  }
+
+  use {
+       'folke/trouble.nvim',
+       version = '2.*',
+       requires = { 'nvim-tree/nvim-web-devicons' },
+       config = function()
+            require("trouble").setup({
+                icons = false,
+            })
+
+            vim.keymap.set("n", "<leader>tt", function()
+                require("trouble").toggle()
+            end)
+
+            vim.keymap.set("n", "[t", function()
+                require("trouble").next({skip_groups = true, jump = true});
+            end)
+
+            vim.keymap.set("n", "]t", function()
+                require("trouble").previous({skip_groups = true, jump = true});
+            end)
+
+        end
+  }
+
+end)
+
 
