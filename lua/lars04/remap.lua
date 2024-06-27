a8924bac4a78e714478e9a7140d698e64c68e185
Lars Loonen <larsvdspek@gmail.com>
2024-06-27 22:30:30 +0200

    Added some remaps

--- lua/lars04/remap.lua
+++ lua/lars04/remap.lua
@@ -3,18 +3,22 @@ local map = vim.keymap.set
 -- Editing
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
+map('i', '<C-H>', '<C-W>', {noremap = true}) -- Sets <C-BS> to <C-W>
 
 map("n", "<C-a>", "ggVG")
+map("v", "y", "ygv<esc>", { remap = true })
 map({ "n", "v" }, "<leader>y", "\"*y")
 map({ "n", "v" }, "<leader>p", "\"*p")
 
 map("n", "<leader>f", vim.lsp.buf.format)
 
--- Commenting (can't get it to work yet, "gc" works tho)
-map("n", "<C-_>", "Vgc")
-map("v", "<C-_>", "gc")
+-- Commenting 
+map("n", "<C-_>", "Vgc", {remap = true})
+map("v", "<C-_>", "gc", {remap = true})
 
 -- Navigating
+map("n", "H", "^", { remap=true })
+map("n", "L", "$", { remap=true })
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 map("n", "n", "nzzzv")
