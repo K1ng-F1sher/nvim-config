4d7a80e801b3589939850e1ad4fa302502b75ed1
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-07-07 21:41:06 +0200

    Configured Trouble and disabled showing hint text

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,39 +56,4 @@ return {
       { 'rafamadriz/friendly-snippets' }
     }
   },
-  {
-    'folke/trouble.nvim',
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    cmd = "Trouble",
-    keys = {
-      {
-        "<leader>tt",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-    }
-  }
-  -- {
-  --   'folke/trouble.nvim',
-  --   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  --
-  --   config = function()
-  --     require("trouble").setup({
-  --       icons = false,
-  --     })
-  --
-  --     vim.keymap.set("n", "<leader>tt", function()
-  --       require("trouble").toggle()
-  --     end)
-  --
-  --     vim.keymap.set("n", "[t", function()
-  --       require("trouble").next({ skip_groups = true, jump = true });
-  --     end)
-  --
-  --     vim.keymap.set("n", "]t", function()
-  --       require("trouble").previous({ skip_groups = true, jump = true });
-  --     end)
-  --   end
-  -- },
 }
