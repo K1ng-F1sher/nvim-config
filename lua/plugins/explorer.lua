af1d2f87d67373af688888d8379043c39737d5d4
Lars Loonen <larsvdspek@gmail.com>
2024-07-15 22:39:50 +0200

    try out with mini.files

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -3,7 +3,9 @@ return {
     -- 'echasnovski/mini.files',
     -- version = '*',
     -- config = function()
+    --   require('mini.files').setup()
     --
+    --   vim.keymap.set("n", "<leader>m", "<cmd>lua MiniFiles.open()<CR>")
     -- end
   },
 }
