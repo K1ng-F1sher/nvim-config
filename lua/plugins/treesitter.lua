d41cb04794a5cd3f3e074a045e887e00d03b4e5b
Lars Loonen <larsvdspek@gmail.com>
2024-07-24 21:26:54 +0200

    disabled markview

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "javascript", "c", "lua", "vim" },
+        ensure_installed = { "javascript", "c", "lua", "vim", "markdown", "markdown_inline"},
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
