5c85665fc313eda241e7fbebfa74da4f1e2a3a45
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-08-09 08:42:08 +0200

    telescope zz

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,16 @@ return
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup {
+      defaults = {
+        mappings = {
+          n = {
+            ["<CR>"] = require("telescope.actions").select_default + require("telescope.actions").center,
+          }
+        }
+      }
+    }
+
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
     vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
