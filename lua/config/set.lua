1bb1a6f80d869b069c16eec936b75d7e8cbc4704
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-08-21 13:22:48 +0200

    PS as shell

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -39,6 +39,27 @@ vim.g.loaded_netrw = 1
 
 vim.g.loaded_netrwPlugin = 1
 
+if vim.fn.executable('pwsh') == 1 then
+    vim.o.shell = 'pwsh'
+else
+    vim.o.shell = 'powershell'
+end
+
+-- Shell settings
+-- Setting shell command flags
+vim.o.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[\'Out-File:Encoding\']=\'utf8\';'
+
+-- Setting shell redirection
+vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+-- Setting shell pipe
+vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+
+-- Setting shell quote options
+vim.o.shellquote = ''
+vim.o.shellxquote = ''
+
+-- LSP settings
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     signs = {
