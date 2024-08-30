895cf8fd148cc3baa14592f8cf6b64e943a3bfc8
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-08-30 09:42:20 +0200

    increased prettier timeout

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -18,10 +18,10 @@ return {
         graphql = { "prettier" },
         lua = { "stylua" },
       },
-       format_on_save = {
+      format_on_save = {
         lsp_fallback = true,
         async = false,
-        timeout_ms = 500,
+        timeout_ms = 1000,
       },
     })
 
@@ -32,7 +32,5 @@ return {
     --     timeout_ms = 500,
     --   })
     -- end, { desc = "Format file or range (in visual mode)" })
-
   end
 }
-
