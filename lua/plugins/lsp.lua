12b818c90ea18294773c7f50c3e99845e3f986cb
Lars Loonen <larsvdspek@gmail.com>
2024-10-02 10:57:55 +0200

    removed unused lines

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -69,8 +69,6 @@ return {
 		lsp.on_attach(function(_, bufnr)
 			local opts = { buffer = bufnr, remap = false }
 
-			-- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-			-- vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
 			vim.keymap.set("n", "K", function()
 				vim.lsp.buf.hover()
 			end, opts)
