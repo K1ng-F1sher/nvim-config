0160d5f39b1920d496cea2ba5931cff290f21c35
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-10-04 16:18:16 +0200

    lazy config for hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -4,9 +4,9 @@ return
   version = '*',
   config = function()
     require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-
     vim.keymap.set("", "S", "<cmd>HopWord<CR>", { noremap = false })
-    vim.keymap.set("", "<Leader>j", "<cmd>HopLineAC<CR>", { noremap = true })
-    vim.keymap.set("", "<Leader>k", "<cmd>HopLineBC<CR>", { noremap = true })
-  end
+  end,
+  keys = {
+    "S"
+  }
 }
