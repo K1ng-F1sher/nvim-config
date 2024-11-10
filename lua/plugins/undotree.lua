0eebf12aebdc4bd9b9555fad6bcc805fc512d630
Lars Loonen <larsvdspek@gmail.com>
2024-11-10 21:22:41 +0100

    potential fix for undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,9 +1,17 @@
 return {
-  "mbbill/undotree",
-  config = function()
-    vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-  end,
-  keys = {
-    "<leader>u",
-  }
+	"mbbill/undotree",
+	lazy = true,
+	cmd = { "UndotreeToggle", "UndotreeShow" },
+	keys = {
+		{
+			"<leader>u",
+			vim.cmd.UndotreeToggle,
+			desc = "Undotree: Toggle Undotree",
+		},
+	},
+	config = function(_, _)
+		if vim.fn.has("win32") == 1 then
+			vim.g.undotree_DiffCommand = "FC"
+		end
+	end,
 }
