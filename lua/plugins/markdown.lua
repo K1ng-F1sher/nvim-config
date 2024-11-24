6696617e4bca45558b4a8cabcccd86fc4579ca6a
Lars Loonen <larsvdspek@gmail.com>
2024-11-24 21:11:47 +0100

    formatted markdown config

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,29 +1,25 @@
 return {
-  "OXY2DEV/markview.nvim",
-  lazy = false,
-  ft = "markdown",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local markview = require("markview")
-    local presets = require("markview.presets")
+	"OXY2DEV/markview.nvim",
+	lazy = false,
+	ft = "markdown",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("markview").setup({
+			modes = { "n", "i", "no", "c" },
+			hybrid_modes = { "i" },
 
-    require("markview").setup({
-      modes = { "n", "i", "no", "c" },
+			-- This is nice to have
+			-- callbacks = {
+			--     on_enable = function(_, win)
+			--         vim.wo[win].conceallevel = 2
+			--         vim.wo[win].concealcursor = "c"
+			--     end,
+			-- },
+		})
 
-      hybrid_modes = { "i" },
-
-      -- This is nice to have
-      callbacks = {
-        on_enable = function(_, win)
-          vim.wo[win].conceallevel = 2
-          vim.wo[win].concealcursor = "c"
-        end,
-      },
-    })
-
-    vim.cmd("Markview enableAll")
-  end,
+		vim.cmd("Markview enableAll")
+	end,
 }
