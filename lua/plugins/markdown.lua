d28136e32c03173f46bc84f8e4a47f129bb6bf3a
Lars Loonen <larsvdspek@gmail.com>
2024-11-29 23:01:09 +0100

    cleaned up markview, removed hybrid mode

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -9,15 +9,6 @@ return {
 	config = function()
 		require("markview").setup({
 			modes = { "n", "i", "no", "c" },
-			hybrid_modes = { "i" },
-
-			-- This is nice to have
-			-- callbacks = {
-			--     on_enable = function(_, win)
-			--         vim.wo[win].conceallevel = 2
-			--         vim.wo[win].concealcursor = "c"
-			--     end,
-			-- },
 		})
 
 		vim.cmd("Markview enableAll")
