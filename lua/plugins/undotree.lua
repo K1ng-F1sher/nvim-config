92cb67126aaf51636ecb96043d1b354c1863cf31
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-12-07 22:51:25 +0100

    changed git keymap and undotree config

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,17 +1,20 @@
 return {
-	"mbbill/undotree",
-	lazy = true,
-	cmd = { "UndotreeToggle", "UndotreeShow" },
-	keys = {
-		{
-			"<leader>u",
-			vim.cmd.UndotreeToggle,
-			desc = "Undotree: Toggle Undotree",
-		},
-	},
-	config = function(_, _)
-		if vim.fn.has("win32") == 1 then
-			vim.g.undotree_DiffCommand = "FC"
-		end
-	end,
+  "mbbill/undotree",
+  lazy = true,
+  cmd = { "UndotreeToggle", "UndotreeShow" },
+  keys = {
+    {
+      "<leader>u",
+      vim.cmd.UndotreeToggle,
+      desc = "Undotree: Toggle Undotree",
+    },
+  },
+  config = function(_, _)
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.g.undotree_DiffAutoOpen = 0
+
+    if vim.fn.has("win32") == 1 then
+      vim.g.undotree_DiffCommand = "FC"
+    end
+  end,
 }
