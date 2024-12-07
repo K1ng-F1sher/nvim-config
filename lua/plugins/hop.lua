e5954592ddcc4848c6f93c0298c8524a6425c0d6
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-12-07 22:32:01 +0100

    Changed hop into 2 chars, updated plugins

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,11 +1,11 @@
 return {
-	"smoka7/hop.nvim",
-	version = "*",
-	config = function()
-		require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-		vim.keymap.set("", "S", "<cmd>HopWord<CR>", { noremap = false })
-	end,
-	keys = {
-		"S",
-	},
+  "smoka7/hop.nvim",
+  version = "*",
+  config = function()
+    require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+    vim.keymap.set("", "S", "<cmd>HopChar2<CR>", { noremap = false })
+  end,
+  keys = {
+    "S",
+  },
 }
