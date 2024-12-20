81b61c9d547012f16f21ac1d43f2d81b6c752e87
Lars Loonen <larsvdspek@gmail.com>
2024-12-20 22:32:37 +0100

    silenced some git commands

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,16 +7,17 @@ return {
       vim.keymap.set("n", "<leader>ga", ":G add .<CR>")
       vim.keymap.set("n", "<leader>gc", ":G commit -m ''<Left>")
       vim.keymap.set("n", "<leader>gq", ":G commit . -m ''<Left>", { desc = "Remember by 'Git Quick commit'." })
-      vim.keymap.set("n", "<leader>gp", ":G push<CR>", { silent = true })
+      vim.keymap.set("n", "<leader>gp", ":G push<CR>")
       vim.keymap.set(
         "n",
         "<leader>gl",
         ":G log --all --graph --decorate --oneline<CR><C-w>o",
-        { desc = "Show a tree of commit history" }
+        { silent = true, desc = "Show a tree of commit history" }
       )
       vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR><C-w>l", {
+        silent = true,
         desc =
-        "Get the two-way diff view for the current buffer and focus the split with new changes. Easiest way to exit: `<C-w>o`",
+        "Get the two-way diff view for the current buffer and focus the split with new changes. Exit with: `<C-w>o`",
       })
     end,
   },
