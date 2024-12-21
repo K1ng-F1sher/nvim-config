808c882b10a7f101437393ddc1073ca6f142f0d9
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-12-21 21:13:13 +0100

    added git-advanced-search to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,59 +11,103 @@ end
 
 local telescope = require("telescope")
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  dependencies = {
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      -- If the line below doesn't work, cd to %LOCALAPPDATA%/nvim-data/lazy/telescope-fzf and execute `make`
-      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
-      config = function()
-        telescope.load_extension("fzf")
-      end,
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VeryLazy",
+    tag = "0.1.8",
+    dependencies = {
+      {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", lazy = false },
+        -- If the line below doesn't work, cd to %LOCALAPPDATA%/nvim-data/lazy/telescope-fzf and execute `make`
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+        config = function()
+          telescope.load_extension("fzf")
+        end,
+      },
     },
-    { "nvim-lua/plenary.nvim" },
-  },
-  config = function()
-    local actions = require("telescope.actions")
+    config = function()
+      local actions = require("telescope.actions")
+      local builtin = require("telescope.builtin")
 
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<CR>"] = actions.select_default + actions.center,
-            ["<BS>"] = false,
-            ["<C-u>"] = false, -- <C-u> now clears the prompt.
-          },
-          n = {
-            ["<CR>"] = actions.select_default + actions.center,
+      telescope.setup({
+        defaults = {
+          path_display = { "smart" },
+          mappings = {
+            i = {
+              ["<CR>"] = actions.select_default + actions.center,
+              ["<BS>"] = false,
+              ["<C-u>"] = false, -- <C-u> now clears the prompt.
+            },
+            n = {
+              ["<CR>"] = actions.select_default + actions.center,
+            },
           },
         },
-      },
-    })
+        extensions = {
+          advanced_git_search = {
+            browse_command = "GBrowse {commit_hash}",
+            diff_plugin = "fugitive",                -- one of fugitive or diffview
+            show_builtin_git_pickers = false,
+            entry_default_author_or_date = "author", -- one of "author" or "date"
+            keymaps = {
+              toggle_date_author = "<C-w>",
+              open_commit_in_browser = "<C-o>",
+              copy_commit_hash = "<C-y>",
+              show_entire_commit = "<C-e>",
+            },
+            telescope_theme = {
+              function_name_1 = {
+                -- Theme options
+              },
+              function_name_2 = "dropdown",
+              -- e.g. realistic example
+              show_custom_functions = {
+                layout_config = { width = 0.4, height = 0.4 },
+              },
+            }
+          }
+        }
+      })
 
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
-    vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>gs", function()
-      centerCallback()
-      builtin.git_status()
-    end, {})
-    vim.keymap.set("n", ";", builtin.resume, {})
-    vim.keymap.set("n", "gr", function()
-      centerCallback()
-      builtin.lsp_references()
-    end, { desc = "Show a list of all references and center when selecting one of them" })
-    vim.keymap.set("n", "gi", function()
-      centerCallback()
-      builtin.lsp_implementations()
-    end, { desc = "Show a list of all imlpementations and center when selecting one of them" })
-    vim.keymap.set("n", "gd", function()
-      centerCallback()
-      builtin.lsp_definitions()
-    end, { desc = "Show a list of all definitions and center when selecting one of them" })
-    vim.keymap.set("n", "gt", builtin.lsp_type_definitions, {})
-    vim.keymap.set("n", "gq", builtin.quickfix, {})
-  end,
+      vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>gs", function() builtin.git_status() end, {})
+      vim.keymap.set("n", ";", builtin.resume, {})
+      vim.keymap.set("n", "gr", function()
+        centerCallback()
+        builtin.lsp_references()
+      end, { desc = "Show a list of all references and center when selecting one of them" })
+      vim.keymap.set("n", "gi", function()
+        centerCallback()
+        builtin.lsp_implementations()
+      end, { desc = "Show a list of all imlpementations and center when selecting one of them" })
+      vim.keymap.set("n", "gd", function()
+        centerCallback()
+        builtin.lsp_definitions()
+      end, { desc = "Show a list of all definitions and center when selecting one of them" })
+      vim.keymap.set("n", "gt", builtin.lsp_type_definitions, {})
+      vim.keymap.set("n", "gq", builtin.quickfix, {})
+    end,
+  },
+  {
+    "aaronhallaert/advanced-git-search.nvim",
+    cmd = { "AdvancedGitSearch" },
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "tpope/vim-fugitive",
+      "tpope/vim-rhubarb",
+      "cedarbaum/fugitive-azure-devops.vim",
+    },
+    config = function()
+      require("telescope").load_extension("advanced_git_search")
+    end,
+    keys = {
+      {
+        "<leader>gh",
+        "<cmd>AdvancedGitSearch search_log_content<cr>",
+        desc = "AdvancedGitSearch through git commit history",
+      }
+    },
+  }
 }
