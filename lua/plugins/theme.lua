5c6526e6613a4e172ab1deebee08c9b4fb90e419
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-12-22 20:43:56 +0100

    updated rosepine theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -9,6 +9,7 @@ return {
         disable_italics = true,
         styles = {
           italic = false,
+          transparency = true
         },
       })
 
@@ -30,9 +31,9 @@ return {
     as = 'tokyonight',
     config = function()
       require("tokyonight").setup({
-        style = "storm",                -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        transparent = true,             -- Enable this to disable setting the background color
-        terminal_colors = true,         -- Configure the colors used when opening a `:terminal` in Neovim
+        style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true,     -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
         disable_background = true,
         styles = {
           italic = false,
@@ -41,13 +42,12 @@ return {
           comments = { italic = false },
           keywords = { italic = false },
           -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "transparent",           -- style for sidebars, see below
-          floats = "transparent",             -- style for floating windows
+          sidebars = "transparent", -- style for sidebars, see below
+          floats = "transparent",   -- style for floating windows
         },
       })
 
       vim.cmd("colorscheme tokyonight")
-
     end
   }
 }
