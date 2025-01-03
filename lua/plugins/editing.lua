52d9a8d889a034d12d09ead34e61305269d80df5
Lars Loonen <larsvdspek@gmail.com>
2025-01-03 20:41:30 +0100

    feat: added editing.lua

---
+++ lua/plugins/editing.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          -- Conform will run the first available formatter
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescriptreact = { "prettier" },
+          css = { "prettier" },
+          scss = { "prettier" },
+          html = { "prettier" },
+          json = { "prettier" },
+          yaml = { "prettier" },
+          markdown = { "prettier" },
+          graphql = { "prettier" },
+          -- lua = { "stylua" },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        },
+      })
+    end,
+  },
+  {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.2") == 1,
+  },
+  {
+    "Wansmer/treesj",
+    keys = {
+      "<leader>m",
+    },
+    dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+    opts = {},
+  },
+  {
+    'echasnovski/mini.nvim',
+    version = "*",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require('mini.ai').setup()
+      require("mini.surround").setup()
+    end,
+  },
+}
