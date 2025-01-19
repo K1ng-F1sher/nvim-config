b2ef2b941f35a21a98cedddac6829cc354fba70e
Lars Loonen <lars.van.der.spek@vandoren.nl>
2025-01-19 23:15:19 +0100

    feat: moved all pickers but 1 to snacks, now having an issue in live grep

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -30,7 +30,12 @@ return
       win = {
         input = {
           keys = {
+            ["<c-f>"] = { "list_scroll_down", mode = { "i", "n" } },    -- Forward
+            ["<c-b>"] = { "list_scroll_up", mode = { "i", "n" } },      -- Back
+            ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },   -- Up
+            ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } }, -- Down
             ["<C-c>"] = { "close", mode = { "n", "i" } },
+            ["<C-h>"] = { "toggle_hidden", mode = { "i", "n" } },
           }
         }
       }
@@ -39,11 +44,24 @@ return
   },
   keys = {
     -- picker
-    { "<C-p>",      function() Snacks.picker.files() end,      desc = "Find Files" },
-    { "<leader>lg", function() Snacks.picker.grep() end,       desc = "Grep" },
-    { ";",          function() Snacks.picker.resume() end,     desc = "Resume" },
-    { "<leader>gc", function() Snacks.picker.git_log() end,    desc = "Git Log" },
-    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+    ---- find
+    { "<C-p>",      function() Snacks.picker.files() end,                desc = "Find Files" },
+    { "<leader>ls", function() Snacks.picker.buffers() end,              desc = "Buffers" },
+    { ";",          function() Snacks.picker.resume() end,               desc = "Resume" },
+    ---- git
+    { "<leader>gl", function() Snacks.picker.git_log() end,              desc = "Git Log" },
+    { "<leader>gs", function() Snacks.picker.git_status() end,           desc = "Git Status" },
+    ---- search
+    { "<leader>lg", function() Snacks.picker.grep() end,                 desc = "Live Grep" },
+    { "<leader>sj", function() Snacks.picker.jumps() end,                desc = "Search Jumps" },
+    { "<leader>sq", function() Snacks.picker.qflist() end,               desc = "Quickfix List" },
+    ---- LSP
+    { "gd",         function() Snacks.picker.lsp_definitions() end,      desc = "Goto Definition" },
+    { "gr",         function() Snacks.picker.lsp_references() end,       nowait = true,                desc = "References" },
+    { "gi",         function() Snacks.picker.lsp_implementations() end,  desc = "Goto Implementation" },
+    { "gt",         function() Snacks.picker.lsp_type_definitions() end, desc = "Goto Type Definition" },
+    { "gs",         function() Snacks.picker.lsp_symbols() end,          desc = "LSP Symbols" },
+
     -- words
     {
       ']]',
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,102 +9,102 @@ local centerCallback = function()
   })
 end
 
-local telescope = require("telescope")
+-- local telescope = require("telescope")
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    event = "VeryLazy",
-    tag = "0.1.8",
-    dependencies = {
-      {
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-live-grep-args.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", lazy = false },
-        -- If the line below doesn't work, cd to %LOCALAPPDATA%/nvim-data/lazy/telescope-fzf and execute `make`
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
-        config = function()
-          telescope.load_extension("fzf")
-          telescope.load_extension("live_grep_args")
-        end,
-      },
-    },
-    config = function()
-      local actions = require("telescope.actions")
-      local builtin = require("telescope.builtin")
-
-      telescope.setup({
-        defaults = {
-          layout_config = {
-            horizontal = { width = 0.95, height = 0.95 }
-          },
-          path_display = { "smart" },
-          mappings = {
-            i = {
-              ["<CR>"] = actions.select_default + actions.center,
-            },
-            n = {
-              ["<CR>"] = actions.select_default + actions.center,
-            },
-          },
-        },
-        pickers = {
-          find_files = {
-            prompt_prefix = "file > "
-          },
-          live_grep = {
-            prompt_prefix = "grep > "
-          },
-          git_status = {
-            prompt_prefix = "git > "
-          }
-        },
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            },
-          },
-          advanced_git_search = {
-            browse_command = "GBrowse {commit_hash}",
-            diff_plugin = "fugitive",                -- one of fugitive or diffview
-            show_builtin_git_pickers = false,
-            entry_default_author_or_date = "author", -- one of "author" or "date"
-            keymaps = {
-              toggle_date_author = "<C-a>",
-              open_commit_in_browser = "<C-o>",
-              copy_commit_hash = "<C-y>",
-              show_entire_commit = "<C-e>",
-            },
-          },
-          live_grep_args = {
-            prompt_prefix = "grep > "
-          }
-        }
-      })
-
-      require("telescope").load_extension("ui-select")
-
-      -- Map("n", "<leader>lg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {})
-      -- Map("n", "<C-p>", builtin.find_files, {})
-      Map('n', '<leader>ls', builtin.buffers, { desc = 'Telescope buffers' })
-      Map("n", "<leader>gs", function() builtin.git_status() end, {})
-      -- Map("n", ";", builtin.resume, {})
-      Map("n", "gr", function()
-        centerCallback()
-        builtin.lsp_references({ show_line = false })
-      end, { desc = "Show a list of all references and center when selecting one of them" })
-      Map("n", "gi", function()
-        centerCallback()
-        builtin.lsp_implementations()
-      end, { desc = "Show a list of all imlpementations and center when selecting one of them" })
-      Map("n", "gd", function()
-        centerCallback()
-        builtin.lsp_definitions()
-      end, { desc = "Show a list of all definitions and center when selecting one of them" })
-      Map("n", "gt", builtin.lsp_type_definitions, {})
-      Map("n", "gs", builtin.treesitter, { desc = "Go to Symbols" })
-      Map("n", "gq", builtin.quickfix, {})
-    end,
-  },
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   -- event = "VeryLazy",
+  --   event = { "BufReadPre", "BufNewFile" },
+  --   tag = "0.1.8",
+  --   dependencies = {
+  --     {
+  --       "nvim-lua/plenary.nvim",
+  --       { "nvim-telescope/telescope-fzf-native.nvim", lazy = false },
+  --       -- If the line below doesn't work, cd to %LOCALAPPDATA%/nvim-data/lazy/telescope-fzf and execute `make`
+  --       build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+  --       config = function()
+  --         telescope.load_extension("fzf")
+  --         telescope.load_extension("live_grep_args")
+  --       end,
+  --     },
+  --   },
+  --   config = function()
+  --     local actions = require("telescope.actions")
+  --     local builtin = require("telescope.builtin")
+  --
+  --     telescope.setup({
+  --       defaults = {
+  --         layout_config = {
+  --           horizontal = { width = 0.95, height = 0.95 }
+  --         },
+  --         path_display = { "smart" },
+  --         mappings = {
+  --           i = {
+  --             ["<CR>"] = actions.select_default + actions.center,
+  --           },
+  --           n = {
+  --             ["<CR>"] = actions.select_default + actions.center,
+  --           },
+  --         },
+  --       },
+  --       pickers = {
+  --         find_files = {
+  --           prompt_prefix = "file > "
+  --         },
+  --         live_grep = {
+  --           prompt_prefix = "grep > "
+  --         },
+  --         git_status = {
+  --           prompt_prefix = "git > "
+  --         }
+  --       },
+  --       extensions = {
+  --         ["ui-select"] = {
+  --           require("telescope.themes").get_dropdown {
+  --           },
+  --         },
+  --         advanced_git_search = {
+  --           browse_command = "GBrowse {commit_hash}",
+  --           diff_plugin = "fugitive",                -- one of fugitive or diffview
+  --           show_builtin_git_pickers = false,
+  --           entry_default_author_or_date = "author", -- one of "author" or "date"
+  --           keymaps = {
+  --             toggle_date_author = "<C-a>",
+  --             open_commit_in_browser = "<C-o>",
+  --             copy_commit_hash = "<C-y>",
+  --             show_entire_commit = "<C-e>",
+  --           },
+  --         },
+  --         live_grep_args = {
+  --           prompt_prefix = "grep > "
+  --         }
+  --       }
+  --     })
+  --
+  --     telescope.load_extension("ui-select")
+  --
+  --     -- Map("n", "<leader>lg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {})
+  --     -- Map("n", "<C-p>", builtin.find_files, {})
+  --     -- Map('n', '<leader>ls', builtin.buffers, { desc = 'Telescope buffers' })
+  --     -- Map("n", "<leader>gs", function() builtin.git_status() end, {})
+  --     -- Map("n", ";", builtin.resume, {})
+  --     -- Map("n", "gr", function()
+  --     --   centerCallback()
+  --     --   builtin.lsp_references({ show_line = false })
+  --     -- end, { desc = "Show a list of all references and center when selecting one of them" })
+  --     -- Map("n", "gi", function()
+  --     --   centerCallback()
+  --     --   builtin.lsp_implementations()
+  --     -- end, { desc = "Show a list of all imlpementations and center when selecting one of them" })
+  --     -- Map("n", "gd", function()
+  --     --   centerCallback()
+  --     --   builtin.lsp_definitions()
+  --     -- end, { desc = "Show a list of all definitions and center when selecting one of them" })
+  --     -- Map("n", "gt", builtin.lsp_type_definitions, {})
+  --     -- Map("n", "gs", builtin.treesitter, { desc = "Go to Symbols" })
+  --     -- Map("n", "gq", builtin.quickfix, {})
+  --   end,
+  -- },
   {
     "aaronhallaert/advanced-git-search.nvim",
     cmd = { "AdvancedGitSearch" },
@@ -115,6 +115,18 @@ return {
       "cedarbaum/fugitive-azure-devops.vim",
     },
     config = function()
+      require("telescope").setup({
+        browse_command = "GBrowse {commit_hash}",
+        diff_plugin = "fugitive",                -- one of fugitive or diffview
+        show_builtin_git_pickers = false,
+        entry_default_author_or_date = "author", -- one of "author" or "date"
+        keymaps = {
+          toggle_date_author = "<C-a>",
+          open_commit_in_browser = "<C-o>",
+          copy_commit_hash = "<C-y>",
+          show_entire_commit = "<C-e>",
+        },
+      })
       require("telescope").load_extension("advanced_git_search")
     end,
     keys = {
