a5847084b7b3936ca855e92e13bc9e767208d3b6
Lars Loonen <lars.van.der.spek@vandoren.nl>
2025-01-19 23:28:31 +0100

    disabled git status in dashboard - displays false info sometimes

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -11,19 +11,19 @@ return
         { section = "header" },
         { section = "keys", padding = 1 },
         { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        {
-          icon = " ",
-          title = "Git Status",
-          section = "terminal",
-          enabled = function()
-            return Snacks.git.get_root() ~= nil
-          end,
-          cmd = "git status --short --branch --renames",
-          height = 5,
-          padding = 1,
-          ttl = 5 * 60,
-          indent = 3,
-        },
+        -- {
+        --   icon = " ",
+        --   title = "Git Status",
+        --   section = "terminal",
+        --   enabled = function()
+        --     return Snacks.git.get_root() ~= nil
+        --   end,
+        --   cmd = "git status --short --branch --renames",
+        --   height = 5,
+        --   padding = 1,
+        --   ttl = 5 * 60,
+        --   indent = 3,
+        -- },
         { section = "startup" },
       },
     },
