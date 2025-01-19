c84a402ec2bd538154529f774c2e2babcee67095
Lars Loonen <lars.van.der.spek@vandoren.nl>
2025-01-19 20:37:08 +0100

    feat: changed markdown viewer to render-markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,28 +1,19 @@
 return {
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   build = "cd app && npm install",
+  --   init = function()
+  --     vim.g.mkdp_filetypes = { "markdown" }
+  --   end,
+  --   ft = { "markdown" },
+  -- },
   {
-    -- "OXY2DEV/markview.nvim",
-    -- lazy = false,
-    -- ft = "markdown",
-    -- dependencies = {
-    --   "nvim-treesitter/nvim-treesitter",
-    --   "nvim-tree/nvim-web-devicons",
-    -- },
-    -- config = function()
-    --   require("markview").setup({
-    --     modes = { "n", "i", "no", "c" },
-    --     hybrid_modes = { 'i' },
-    --   })
-    --
-    --   vim.cmd("Markview enableAll")
-    -- end,
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
+    cmd = { "RenderMarkdown" },
+    event = { "BufReadPre", "BufNewFile" },
+    ---@module 'render-markdown'
+    opts = {},
   }
 }
