a2c2979bb89b865b88f527f8fb0069765c7dfe91
Lars Loonen <larsvdspek@gmail.com>
2025-01-26 19:50:31 +0100

    updated plugins and configured pairs

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -50,13 +50,25 @@ return {
       require("mini.pairs").setup(
         {
           mappings = {
-            ['('] = { action = 'open', pair = '()', neigh_pattern = '.[^%a%d]' },
+            ['('] = {
+              action = 'open',
+              pair = '()',
+              neigh_pattern = '.[^%a%d]',
+              { desc = "Don't open bracket when cursor is before an alphanumeric character" }
+            },
             ['['] = { action = 'open', pair = '[]', neigh_pattern = '.[^%a%d]' },
             ['{'] = { action = 'open', pair = '{}', neigh_pattern = '.[^%a%d]' },
 
             ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '.[^%a%d]', register = { cr = false } },
             ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '.[^%a%d]', register = { cr = false } },
-          }
+          },
+          -- skip autopair when next character is one of these
+          skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+          -- skip autopair when next character is closing pair
+          -- and there are more closing pairs than opening pairs
+          skip_unbalanced = true,
+          -- better deal with markdown code blocks
+          markdown = true,
         }
       )
       require("mini.surround").setup()
