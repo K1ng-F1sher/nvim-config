3d4416170e579b633d67d320b393043bc74f2a28
Lars Loonen <larsvdspek@gmail.com>
2025-03-01 22:28:12 +0100

    feat: implemented blink.cmp

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -1,33 +1,50 @@
 return {
   {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
+    'neovim/nvim-lspconfig',
+    event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" },
-      { "williamboman/mason.nvim" },
-      { "williamboman/mason-lspconfig.nvim" },
-
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" },
-      { "hrsh7th/cmp-buffer" },
-      { "hrsh7th/cmp-path" },
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-nvim-lua" },
-      { "saadparwaiz1/cmp_luasnip" },
-
-      -- Snippets
-      { "L3MON4D3/LuaSnip" },
-      { "rafamadriz/friendly-snippets" },
+      -- LSP wrapper for vtsls.
+      'yioneko/nvim-vtsls',
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      'saghen/blink.cmp'
     },
     config = function()
-      local lsp = require("lsp-zero")
+      local capabilities = nil
+      if pcall(require, "cmp_nvim_lsp") then
+        capabilities = require("cmp_nvim_lsp").default_capabilities()
+      end
 
-      lsp.preset("recommended")
+      local lspconfig = require "lspconfig"
 
-      require("mason").setup({})
+      local servers = {
+        bashls = true,
+        lua_ls = true,
+        rust_analyzer = true,
+        cssls = true,
 
-      require("mason-lspconfig").setup({
+        -- Probably want to disable formatting for this lang server
+        ts_ls = true,
+
+        jsonls = {
+          settings = {
+            json = {
+              validate = { enable = true },
+            },
+          },
+        },
+      }
+
+      local servers_to_install = vim.tbl_filter(function(key)
+        local t = servers[key]
+        if type(t) == "table" then
+          return not t.manual_install
+        else
+          return t
+        end
+      end, vim.tbl_keys(servers))
+
+      require("mason").setup({
         ensure_installed = {
           "ts_ls",
           "eslint",
@@ -35,7 +52,6 @@ return {
         },
         automatic_installation = true,
         handlers = {
-          lsp.default_setup,
 
           powershell_es = function()
             local lspconfig = require("lspconfig")
@@ -49,38 +65,179 @@ return {
         },
       })
 
-      local cmp = require("cmp")
-      local cmp_select = { behavior = cmp.SelectBehavior.Select }
-      cmp.setup({
-        mapping = cmp.mapping.preset.insert({
-          ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-          ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-        }),
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        sign_icons = {},
+      for name, config in pairs(servers) do
+        if config == true then
+          config = {}
+        end
+        config = vim.tbl_deep_extend("force", {}, {
+          capabilities = capabilities,
+        }, config)
+
+        lspconfig[name].setup(config)
+      end
+
+      local disable_semantic_tokens = {
+        lua = true,
+      }
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local bufnr = args.buf
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+
+          Map("n", "K", function() vim.lsp.buf.hover() end, opts)
+          Map("n", "gl", function() vim.diagnostic.open_float() end, opts)
+          Map("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+          Map("n", "<leader>vd", function() vim.diagnostics.open_float() end, opts)
+          Map("n", "[d", function() vim.diagnostics.goto_next() end, opts)
+          Map("n", "]d", function() vim.diagnostics.goto_prev() end, opts)
+          Map("n", "<leader>vc", function() vim.lsp.buf.code_action() end, opts)
+          Map("n", "<leader>vh", function() vim.lsp.buf.signature_help() end, opts)
+          Map("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+          local filetype = vim.bo[bufnr].filetype
+          if disable_semantic_tokens[filetype] then
+            client.server_capabilities.semanticTokensProvider = nil
+          end
+        end,
       })
+    end,
+  },
 
-      lsp.extend_lspconfig()
-      lsp.on_attach(function(_, bufnr)
-        local opts = { buffer = bufnr, remap = false }
+  {
+    'saghen/blink.cmp',
+    version = "*",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      -- add blink.compat to dependencies
+      {
+        "saghen/blink.compat",
+        optional = true, -- make optional so it's only enabled if any extras need it
+        opts = {},
+        version = "*",
+      },
+    },
+    event = "InsertEnter",
 
-        Map("n", "K", function() vim.lsp.buf.hover() end, opts)
-        Map("n", "gl", function() vim.diagnostic.open_float() end, opts)
-        Map("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        Map("n", "<leader>vd", function() vim.diagnostics.open_float() end, opts)
-        Map("n", "[d", function() vim.diagnostics.goto_next() end, opts)
-        Map("n", "]d", function() vim.diagnostics.goto_prev() end, opts)
-        Map("n", "<leader>vc", function() vim.lsp.buf.code_action() end, opts)
-        Map("n", "<leader>vh", function() vim.lsp.buf.signature_help() end, opts)
-        Map("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-      end)
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      appearance = {
+        -- sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release, assuming themes add support
+        use_nvim_cmp_as_default = false,
+        -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        accept = {
+          -- experimental auto-brackets support
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+        menu = {
+          border = 'rounded',
+          draw = {
+            treesitter = { "lsp" },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          -- auto_show_delay_ms = 200,
+          window = { border = 'rounded' },
+        },
+        ghost_text = {
+          enabled = vim.g.ai_cmp,
+        },
+      },
 
-      lsp.setup()
+      -- experimental signature help support
+      -- signature = { enabled = true },
+
+      sources = {
+        -- adding any nvim-cmp sources here will enable them
+        -- with blink.compat
+        compat = {},
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      cmdline = {
+        enabled = false,
+      },
+
+      keymap = {
+        ['<CR>'] = { 'accept', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'show' },
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<C-p>'] = { 'select_prev' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      },
+    },
+
+    ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
+    config = function(_, opts)
+      -- setup compat sources
+      local enabled = opts.sources.default
+      for _, source in ipairs(opts.sources.compat or {}) do
+        opts.sources.providers[source] = vim.tbl_deep_extend(
+          "force",
+          { name = source, module = "blink.compat.source" },
+          opts.sources.providers[source] or {}
+        )
+        if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
+          table.insert(enabled, source)
+        end
+      end
+
+      -- add ai_accept to <Tab> key
+      if not opts.keymap["<Tab>"] then
+        if opts.keymap.preset == "super-tab" then -- super-tab
+          opts.keymap["<Tab>"] = {
+            require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
+            "fallback",
+          }
+        else -- other presets
+          opts.keymap["<Tab>"] = {
+            "fallback",
+          }
+        end
+      end
+
+      -- Unset custom prop to pass blink.cmp validation
+      opts.sources.compat = nil
+
+      -- check if we need to override symbol kinds
+      for _, provider in pairs(opts.sources.providers or {}) do
+        ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
+        if provider.kind then
+          local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+          local kind_idx = #CompletionItemKind + 1
+
+          CompletionItemKind[kind_idx] = provider.kind
+          ---@diagnostic disable-next-line: no-unknown
+          CompletionItemKind[provider.kind] = kind_idx
+
+          ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
+          local transform_items = provider.transform_items
+          ---@param ctx blink.cmp.Context
+          ---@param items blink.cmp.CompletionItem[]
+          provider.transform_items = function(ctx, items)
+            items = transform_items and transform_items(ctx, items) or items
+            for _, item in ipairs(items) do
+              item.kind = kind_idx or item.kind
+            end
+            return items
+          end
+
+          -- Unset custom prop to pass blink.cmp validation
+          provider.kind = nil
+        end
+      end
+
+      require("blink.cmp").setup(opts)
     end,
   },
 
