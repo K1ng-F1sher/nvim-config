44c63f686520e18d3d16221f034d4dc0b47e202f
Lars Loonen <larsvdspek@gmail.com>
2025-03-01 22:33:33 +0100

    feat: added signatures

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -154,7 +154,7 @@ return {
       },
 
       -- experimental signature help support
-      -- signature = { enabled = true },
+      signature = { enabled = true },
 
       sources = {
         -- adding any nvim-cmp sources here will enable them
