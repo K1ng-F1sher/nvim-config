07858541b7496b0e862e3cf2d0aadf5baa657fa9
Lars Loonen <larsvdspek@gmail.com>
2025-04-09 22:25:52 +0200

    feat: yank ring and added mapping for listing register contents

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -122,6 +122,23 @@ augroup highlight_yank
 augroup END
 ]])
 
+-- Shift numbered registers up (1 becomes 2, etc.)
+local function yank_shift()
+  for i = 9, 1, -1 do
+    vim.fn.setreg(tostring(i), vim.fn.getreg(tostring(i - 1)))
+  end
+end
+
+-- Create autocmd for TextYankPost event
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    local event = vim.v.event
+    if event.operator == "y" then
+      yank_shift()
+    end
+  end,
+})
+
 -- Set highlighting styles for quickscope
 vim.cmd([[
   highlight QuickScopePrimary gui=underline cterm=underline
--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -1,3 +1,5 @@
+local ivyLayout = { layout = { preset = "ivy", preview = false } }
+
 return {
   {
     "lukas-reineke/indent-blankline.nvim",
@@ -96,10 +98,10 @@ return {
         win = {
           input = {
             keys = {
-              ["<c-f>"] = { "list_scroll_down", mode = { "i", "n" } },    -- Forward
-              ["<c-b>"] = { "list_scroll_up", mode = { "i", "n" } },      -- Back
-              ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },   -- Up
-              ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } }, -- Down
+              ["<c-f>"] = { "list_scroll_down", mode = { "i", "n" }, desc = "Forward" },
+              ["<c-b>"] = { "list_scroll_up", mode = { "i", "n" }, desc = "Back" },
+              ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" }, desc = "Up" },
+              ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" }, desc = "Down" },
               ["<C-c>"] = { "close", mode = { "n", "i" } },
               ["<C-h>"] = { "toggle_hidden", mode = { "i", "n" } },
             }
@@ -109,33 +111,33 @@ return {
       quickfile = {},
       words = {},
 
-
     },
     keys = {
       -- picker
       ---- find
-      { "<C-p>",            function() Snacks.picker.files() end,                desc = "Find Files" },
-      { "<leader>ls",       function() Snacks.picker.buffers() end,              desc = "Buffers" },
-      { "<leader><leader>", function() Snacks.picker.resume() end,               desc = "Resume" },
+      { "<C-p>",            function() Snacks.picker.files() end,                    desc = "find files" },
+      { "<leader>ls",       function() Snacks.picker.buffers() end,                  desc = "buffers" },
+      { "<leader><leader>", function() Snacks.picker.resume() end,                   desc = "resume" },
       ---- git
       -- { "<leader>gl", function() Snacks.picker.git_log() end,              desc = "Git Log" }, use <leader>gt instead
-      { "<leader>gf",       function() Snacks.picker.git_log_file() end,         desc = "Git Log File" },
-      { "<leader>gL",       function() Snacks.picker.git_log_line() end,         desc = "Git Log Line" },
-      { "<leader>gs",       function() Snacks.picker.git_status() end,           desc = "Git Status" },
-      { "<leader>e",        function() Snacks.explorer() end,                    desc = "File Explorer" },
+      { "<leader>gf",       function() Snacks.picker.git_log_file() end,             desc = "Git log File" },
+      { "<leader>gL",       function() Snacks.picker.git_log_line() end,             desc = "Git log Line" },
+      { "<leader>gs",       function() Snacks.picker.git_status() end,               desc = "Git Status" },
+      { "<leader>e",        function() Snacks.explorer() end,                        desc = "file Explorer" },
       ---- search
-      { "<leader>lg",       function() Snacks.picker.grep() end,                 desc = "Live Grep" },
-      { "<leader>jl",       function() Snacks.picker.jumps() end,                desc = "Jump List" },
-      { "<leader>qf",       function() Snacks.picker.qflist() end,               desc = "QuickFix list" },
-      { "<leader>ch",       function() Snacks.picker.command_history() end,      desc = "Command History" },
-      { "<leader>tt",       function() Snacks.picker.diagnostics() end,          desc = "Toggle Trouble (Diagnostics)" },
-      { "<leader>nh",       function() Snacks.picker.notifications() end,        desc = "Notification History" },
+      { "<leader>lg",       function() Snacks.picker.grep() end,                     desc = "Live Grep" },
+      { "<leader>jl",       function() Snacks.picker.jumps() end,                    desc = "Jump List" },
+      { '<leader>rl',       function() Snacks.picker.registers() end,                desc = "Register List" },
+      { "<leader>qf",       function() Snacks.picker.qflist() end,                   desc = "QuickFix list" },
+      { "<leader>ch",       function() Snacks.picker.command_history(ivyLayout) end, desc = "Command History" },
+      { "<leader>nh",       function() Snacks.picker.notifications() end,            desc = "Notification History" },
+      { "<leader>tt",       function() Snacks.picker.diagnostics() end,              desc = "Toggle Trouble (diagnostics)" },
       ---- LSP
-      { "gd",               function() Snacks.picker.lsp_definitions() end,      desc = "Goto Definition" },
-      { "gr",               function() Snacks.picker.lsp_references() end,       nowait = true,                        desc = "References" },
-      { "gi",               function() Snacks.picker.lsp_implementations() end,  desc = "Goto Implementation" },
-      { "gt",               function() Snacks.picker.lsp_type_definitions() end, desc = "Goto Type Definition" },
-      { "gs",               function() Snacks.picker.lsp_symbols() end,          desc = "LSP Symbols" },
+      { "gd",               function() Snacks.picker.lsp_definitions() end,          desc = "Goto Definition" },
+      { "gr",               function() Snacks.picker.lsp_references() end,           nowait = true,                        desc = "Goto References" },
+      { "gi",               function() Snacks.picker.lsp_implementations() end,      desc = "Goto Implementation" },
+      { "gt",               function() Snacks.picker.lsp_type_definitions() end,     desc = "Goto Type definition" },
+      { "gs",               function() Snacks.picker.lsp_symbols() end,              desc = "Goto lsp Symbols" },
 
       -- words
       {
@@ -144,7 +146,7 @@ return {
           Snacks.words.jump(vim.v.count1)
           vim.cmd("norm! zz")
         end,
-        desc = 'Next Reference',
+        desc = 'Next reference',
       },
       {
         '[[',
@@ -152,7 +154,7 @@ return {
           Snacks.words.jump(-vim.v.count1)
           vim.cmd("norm! zz")
         end,
-        desc = 'Prev Reference',
+        desc = 'Prev reference',
       },
     }
   },
