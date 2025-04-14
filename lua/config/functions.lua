a2ba4f88ef07a5cd5ac6afff75fb02d9583f0435
Lars Loonen <larsvdspek@gmail.com>
2025-04-14 16:08:43 +0200

    refactor: moved function outside plugin config

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,6 +1,14 @@
-------------------
---- NO ITALICS ---
-------------------
+---------------
+--- COLOURS ---
+---------------
+function ColorMyPencils(color)
+  color = color or "rose-pine"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 function DisableItalics()
   local hl_groups = vim.api.nvim_get_hl(0, {})
 
