2c48d3361c54bf89490161037b63023e4cbf162e
Lars Loonen <lars.van.der.spek@vandoren.nl>
2026-01-12 10:08:47 +0100

    feat: lazy load smear cursor

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -29,12 +29,13 @@ return {
 
   {
     "sphamba/smear-cursor.nvim",
+    cmd = { "SmearCursorToggle" },
     opts = {
       enabled = false,
       cursor_color = "#ff8800",
       stiffness = 0.3,
       trailing_stiffness = 0.1,
-      trailing_exponent = 5,
+      trailing_exponent = 3,
       never_draw_over_target = true,
       hide_target_hack = true,
       gamma = 1,
